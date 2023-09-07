20556b887ffdeb64018c413fa9166e1761049d28
Jesufemi <oladapofemmy@gmail.com>
2023-09-07 17:49:08 +0100

    fix file 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,5 +13,7 @@ int main(void)
 	printf("Size of a long int: %d byte(s)\n", (int)sizeof(long));
 	printf("Size of a long long int: %d byte(s)\n", (int)sizeof(long long int));
 	printf("Size of a float: %d byte(s)\n", (int)sizeof(float));
+
+	return (0);
 }
 

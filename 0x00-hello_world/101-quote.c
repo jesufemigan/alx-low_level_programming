50a88c8c305f7b6a18b4434166b104698f2e43f0
Jesufemi <oladapofemmy@gmail.com>
2023-09-07 18:47:15 +0100

    fix file 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,16 @@
 #include <unistd.h>
 
-/*
+/**
  * main - Entry Point
  *
- * Return : this will return 1 based on the task
+ * Return: this will return 1 based on the task
  */
 
 int main(void)
 {
-	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char message[] = "and that piece of art is useful\" - Dora Korpar, \
+2015-10-19\n";
+
 	write(2, message, sizeof(message) - 1);
 	return (1);
 }

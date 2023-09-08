cf60490763364f49eb4c5dc84c7c8a31da22a2f7
Jesufemi <oladapofemmy@gmail.com>
2023-09-08 21:11:07 +0100

    task 10 done

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,1 +1,34 @@
 #include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: always return 0
+ */
+
+int main(void)
+{
+	int i, j;
+
+	i = 0;
+
+	while (i <= 8)
+	{
+		j = i + 1;
+		while (j <= 9)
+		{
+			putchar('0' + i);
+			putchar('0' + j);
+
+			if (i != 8 || j != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}

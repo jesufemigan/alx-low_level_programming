4f90f4324af5b167fe5c4e61c1c3a60688333431
Jesufemi <oladapofemmy@gmail.com>
2023-09-08 20:47:24 +0100

    task 7 done

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: always return 0
+ */
+
+int main(void)
+{
+	char letter;
+
+	letter = 'z';
+
+	while (letter >= 'a')
+	{
+		putchar(letter);
+		z--;
+	}
+	putchar('\n);
+	return (0);
+}

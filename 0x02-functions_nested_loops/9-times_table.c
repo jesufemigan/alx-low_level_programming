96e339c0fb9be0558e5d9feb2e1ff4fb02fa35c4
Jesufemi <oladapofemmy@gmail.com>
2023-09-12 16:19:42 +0100

    fix file 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,6 +10,7 @@ void times_table(void)
 {
 	int n;
 	int m;
+	int product;
 
 	n = 0;
 
@@ -18,16 +19,18 @@ void times_table(void)
 		m = 0;
 		while (m <= 9)
 		{
-			_putchar((n * m) + '0');
+			product = n * m;
+			_putchar((product / 10) + '0');
+			_putchar((product % 10) + '0');
 			_putchar(' ');
 			_putchar(' ');
 			if (m != 9)
 			{
 				_putchar(',');
 			}
-			_putchar('\n');
 			m++;
 		}
+		_putchar('\n');
 		n++;
 	}
 }

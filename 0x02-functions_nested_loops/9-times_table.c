8241b662b75487fe6e4dd65758658fba658f7796
Jesufemi <oladapofemmy@gmail.com>
2023-09-12 19:09:46 +0100

    fix space issue for file 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -23,16 +23,22 @@ void times_table(void)
 			if (product <= 9)
 			{
 				_putchar(product + '0');
-				_putchar(' ');
+
+				if (m != 9)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(',');
+				}
 			} else
 			{
-			_putchar((product / 10) + '0');
-			_putchar((product % 10) + '0');
-			}
-			if (m != 9)
-			{
-				_putchar(',');
-				_putchar(' ');
+				_putchar((product / 10) + '0');
+				_putchar((product % 10) + '0');
+				if (m != 9)
+				{
+					_putchar(' ');
+					_putchar(',');
+				}	
 			}
 			m++;
 		}

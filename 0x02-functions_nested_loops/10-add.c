880999121d62a80190a0b393f7a02947451246e9
Jesufemi <oladapofemmy@gmail.com>
2023-09-12 20:41:01 +0100

    task 10 done

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - add two integers
+ * @num1: first number to add
+ * @num2: second number to dd
+ *
+ * Return: value of num1+num2
+ */
+
+int add(int n, int m)
+{
+	return (n + m);
+}

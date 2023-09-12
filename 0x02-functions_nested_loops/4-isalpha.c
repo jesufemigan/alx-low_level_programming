dc446955cc91cb6ba5b72452d55022ec07dd2e96
Jesufemi <oladapofemmy@gmail.com>
2023-09-12 14:55:46 +0100

    task 4 done

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ *
+ * @c: character to checl
+ *
+ * Return: return 1 if c is a letter, lowercase or uppercase, else return 0
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/4-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry Point
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int r;
+
+	r = _isalpha('H');
+
+	return (0);
+}

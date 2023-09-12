2d03cc69c3ae6a3002c9108fa67aecdf0ae397f1
Jesufemi <oladapofemmy@gmail.com>
2023-09-12 13:58:30 +0100

    task 2 done

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet in  a new line
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int n;
+
+	n = 0;
+
+	while (n <= 10)
+	{
+		char letter;
+
+		letter = 'a';
+
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+		n++;
+	}
+	_putchar('\n');
+}

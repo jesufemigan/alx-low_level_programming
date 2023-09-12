9878427be090a6b70ac0af3f6b2c3a6ab31fd463
Jesufemi <oladapofemmy@gmail.com>
2023-09-12 16:05:10 +0100

    fix file 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * jack_bauer - prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59
+ * jack_bauer - prints every minute of the day of Jack Bauer, starting from
+ * 00:00 to 23:59
  *
  * Return: no return
  */
 
-void jack_bauer()
+void jack_bauer(void)
 {
 	int hour;
 	int minute;
@@ -25,7 +26,7 @@ void jack_bauer()
 			_putchar((minute % 10) + '0');
 			minute++;
 		}
-		hour++
+		hour++;
 	}
 	_putchar('\n');
 }

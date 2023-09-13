6a24444171f8c556c924d36d1f2f228f51c93918
Jesufemi <oladapofemmy@gmail.com>
2023-09-13 16:52:11 +0100

    task 0 fix

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * main - entry point
- *
- * Return: always 0
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
  */
 
 int main(void)
@@ -11,7 +10,7 @@ int main(void)
 	int i;
 
 	i = 0;
-
 	positive_or_negative(i);
+
 	return (0);
 }

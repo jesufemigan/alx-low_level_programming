368cd1ceec584f824f356731ed3cdb146aa48846
Jesufemi <oladapofemmy@gmail.com>
2023-09-14 15:04:05 +0100

    task 7 done

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws a diagonal on the terminal
+ * @n: number of diagonal
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int start;
+	int pos;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		start = 1;
+
+		while (start < n)
+		{
+			pos = 1;
+
+			while (pos < start)
+			{
+				_putchar(' ');
+				pos++;
+			}
+			_putchar('\\');
+			_putchar('\n');
+			start++;
+		}
+	}
+}

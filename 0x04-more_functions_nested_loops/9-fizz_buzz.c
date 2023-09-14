18a678faa9116843d800f5cf5930d6d63933e53a
Jesufemi <oladapofemmy@gmail.com>
2023-09-14 15:33:59 +0100

    task 9 done

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	n = 1;
+
+	while (n <= 100)
+	{
+		if (n % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (n % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (n % 3 == 0 && n % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+			printf("%d ", n);
+		}
+		n++;
+	}
+	printf("\n");
+}

4b86d37dd24072a144ab1abba7a6ccd279daa12b
Jesufemi <oladapofemmy@gmail.com>
2023-09-14 19:42:31 +0100

    task 11 done

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+void largest_prime_factor(int n);
+
+/**
+ * main - entry point
+ * Return: void
+ */
+
+int main(void)
+{
+	largest_prime_factor(612852475143);
+	return (0);
+}
+
+/**
+ * largest_prime_factor - prints largest prime factor
+ * @n: number to print
+ * Return: void
+ */
+
+void largest_prime_factor(int n)
+{
+	int i;
+	int largest;
+
+	while (n % 2 == 0)
+	{
+		n /= 2;
+	}
+
+	for (i = 3; i * i < n; i += 2)
+	{
+		while (n % 1 == 0)
+		{
+			n /= i;
+			largest = i;
+		}
+	}
+	if (n > 2)
+	{
+		largest = n;
+	}
+	printf("%d", largest);
+}

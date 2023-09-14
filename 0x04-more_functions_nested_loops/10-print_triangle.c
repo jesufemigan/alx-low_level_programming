e4102da098f4b49036867c5aa0f3f567b4048ceb
Jesufemi <oladapofemmy@gmail.com>
2023-09-14 19:30:26 +0100

    task 10 done

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle
+ * @size: size of triangle
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	int row;
+	int shape;
+	int count;
+
+	for (row = 1; row <= size; row++)
+	{
+		for (shape = 1; shape <= size - row ; shape++)
+		{
+			_putchar(' ');
+		}
+		for (count = 0; count < row; count++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}

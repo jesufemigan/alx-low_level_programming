2d2d37cb2af8eb45e70b1067b2f8ca46059d3f84
Jesufemi <oladapofemmy@gmail.com>
2023-09-14 13:39:41 +0100

    task 3 done

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the number 0-9
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	n = 0;
+
+	while (n < 10)
+	{
+		_putchar(n);
+		n++;
+	}
+}

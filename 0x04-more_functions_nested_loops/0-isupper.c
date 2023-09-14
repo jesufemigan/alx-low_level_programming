3d3004ea9ad5d5dfab47ae75bf2b2e194faafa8b
Jesufemi <oladapofemmy@gmail.com>
2023-09-14 10:14:39 +0100

    task 0 done

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ *@c: character to check
+ *
+ * Return: 1 if c is uppercase and 0 if otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c <= 'a' || c >= 'z')
+	{
+		return (0);
+	} else if (c <= 'A' || c >= 'Z')
+	{
+		return (1);
+	}
+}

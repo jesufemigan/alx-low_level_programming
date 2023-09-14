02c2cb51792c794c7926be6018d0d6b128c649fe
Jesufemi <oladapofemmy@gmail.com>
2023-09-14 13:15:07 +0100

    fix task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -9,10 +9,10 @@
 
 int _isupper(int c)
 {
-	if (c <= 'a' || c >= 'z')
+	if (c >= 'a' && c <= 'z')
 	{
 		return (0);
-	} else if (c <= 'A' || c >= 'Z')
+	} else if (c >= 'A' && c <= 'Z')
 	{
 		return (1);
 	}

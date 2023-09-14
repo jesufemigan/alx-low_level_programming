6e7ff6b257e955b5b28523a0b9f61b8e35fa6e6d
Jesufemi <oladapofemmy@gmail.com>
2023-09-14 14:35:56 +0100

    task 6 done

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - drawa a straight line
+ *@n: number of _
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		while (n > 0)
+		{
+			_putchar('_');
+			n--;
+		}
+		_putchar('\n');
+	}
+}

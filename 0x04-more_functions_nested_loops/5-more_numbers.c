0aa80815c1fc8c7c71bbd3b287e64cda46b73cec
Jesufemi <oladapofemmy@gmail.com>
2023-09-14 14:18:29 +0100

    fx file 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -17,7 +17,15 @@ void more_numbers(void)
 
 		while (n <= 14)
 		{
-			_putchar(n + '0');
+			if ( n < 10)
+			{
+				_putchar(n + '0');
+			}
+			else
+			{
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+			}
 			n++;
 		}
 		_putchar('\n');

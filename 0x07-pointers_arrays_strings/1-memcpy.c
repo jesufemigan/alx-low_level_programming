069950bbff8ce7ca03d4e603aeff023160a592bf
Jesufemi <oladapofemmy@gmail.com>
2023-09-25 16:58:37 +0100

    fix 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,10 +12,14 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	while (n > 0)
+	unsigned int i;
+
+	i = 0;
+
+	while (i < n)
 	{
-		dest[n] = src[n];
-		n--;
+		dest[i] = src[i];
+		i++;
 	}
 
 	return (dest);

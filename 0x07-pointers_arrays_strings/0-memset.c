23f600abd455351cd1912734aa9704540e0241db
Jesufemi <oladapofemmy@gmail.com>
2023-09-25 16:43:32 +0100

    newest fix

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -14,13 +14,11 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	i = 0;
-
-	while (n > 0)
+	for (i = 0; n > 0; i++, n--)
 	{
 		s[i] = b;
-		i++;
-		n--;
 	}
+
 	return (s);
+
 }

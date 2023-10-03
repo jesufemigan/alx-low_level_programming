c9ce5058de3514579ee82c147b9275a53e9b9272
Jesufemi <oladapofemmy@gmail.com>
2023-10-03 03:50:51 +0100

    task 0 done

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * @argc: argument count
+ * @argv[]: vector
+ *
+ * Return: int
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - argc]);
+	return (0);
+}

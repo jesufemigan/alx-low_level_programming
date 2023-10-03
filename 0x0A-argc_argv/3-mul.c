6b8002e5b8532dcd07304cba267c378fd5e92e6d
Jesufemi <oladapofemmy@gmail.com>
2023-10-03 05:02:20 +0100

    task 3 done

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry Point
+ *
+ * @argc: int
+ * @argv: vector
+ *
+ * Return: int
+ */
+
+int main(int argc, char *argv[])
+{
+	int result;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		result = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", result);
+	}
+
+	return (0);
+}

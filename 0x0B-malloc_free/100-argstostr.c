efb9c0188d24908d6400959081f8db25a5435516
Jesufemi <oladapofemmy@gmail.com>
2023-10-05 17:57:14 +0100

    task 5 done

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+int _strlen(char *s);
+
+/**
+ * _strlen - legnth of string
+ *
+ * @s: string
+ *
+ * Return: int
+ */
+
+int _strlen(char *s)
+{
+	int n;
+
+	n = 0;
+
+	while (*s)
+	{
+		n++;
+		s++;
+	}
+	return (n);
+}
+
+/**
+ * argstostr - concatenates all the arguments of the program
+ *
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: char
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int nc, i, j, ni;
+	char *new_string;
+
+	i = 0;
+
+	for (; i < ac; i++, nc++)
+	{
+		nc += _strlen(av[i]);
+	}
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	new_string = malloc(nc * sizeof(char) + 1);
+
+	if (new_string == 0)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++, ni++)
+		{
+			new_string[ni] = av[i][j];
+		}
+		new_string[ni] = '\n';
+		ni++;
+	}
+
+	return (new_string);
+}

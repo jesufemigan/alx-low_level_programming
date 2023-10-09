10112902b4fb0ee8fa0be3a080f2216d8c4669d1
Jesufemi <oladapofemmy@gmail.com>
2023-10-09 19:21:53 +0100

    task 2 done

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdlib.h>
+
+int _memset(char *s, char b, unsigned int n);
+
+/**
+ * _memset - set memory to 0
+ *
+ * @s: pointer
+ * @b: byyes
+ * @n: num
+ *
+ * Return: int
+ */
+
+int _memset(char *s, char b, unsigned int n)
+{
+	char *ptr;
+
+	ptr = s;
+
+	while (n--)
+	{
+		*s = b;
+	}
+	return (ptr);
+}
+
+/**
+ * _calloc - allocates memory for an array
+ *
+ * @nmemb: num
+ * @size: size
+ *
+ * Return: void
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *m;
+
+	m = malloc(sizeof(int) * nmemb);
+
+	if (m == 0)
+		return (NULL);
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	_memset(m, 0, sizeof(int) * nmemb);
+	return (m);
+}

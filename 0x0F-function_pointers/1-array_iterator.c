7165b1707a76a1481d4156541a8b95e2bd7d09ee
Jesufemi <oladapofemmy@gmail.com>
2023-10-11 22:00:17 +0100

    task 1 done

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include <stddef.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes a function given as a parameter
+ *
+ * @array: array
+ * @size: size of array
+ * @action: pointer to function
+ *
+ * Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}

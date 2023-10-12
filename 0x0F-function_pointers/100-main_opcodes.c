b1b7edd963a7b88a25449854b7e0e9a365b8352e
Jesufemi <oladapofemmy@gmail.com>
2023-10-12 01:22:58 +0100

    task 5 done

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * main - entry point
+ *
+ * @argc: count
+ * @argv: vector
+ *
+ * Return: int
+ */
+
+int main(argc, char *argv[])
+{
+	int b;
+	char *p = (char *)main;
+	b = atoi(argv[1]);
+
+	if (argc != 2)
+		printf("Error\n"), exit(1);
+
+	if (b < 0)
+		printf("Error\n"), exit(2);
+
+	while (b--)
+	{
+		printf("%02hhx%s", *p++, b ? " " : "\n");
+	}
+	return (0);
+}

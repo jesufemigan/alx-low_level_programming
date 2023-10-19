9b69737ec516fb0e35ee74e88a92f3842a1d6cc5
Jesufemi <oladapofemmy@gmail.com>
2023-10-19 12:06:41 +0100

    fix

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,7 +1,7 @@
-#include <stddef.h>
-
 #ifndef LIST_H
 #define LIST_H
+
+#include <stddef.h>
 
 int _putchar(char c);
 
@@ -15,9 +15,9 @@ int _putchar(char c);
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);

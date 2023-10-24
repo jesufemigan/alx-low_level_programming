002bbbcd49ed50a3b190ff17181240409d8f2a5b
Jesufemi <oladapofemmy@gmail.com>
2023-10-24 11:56:05 +0100

    task 5 done

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2 - frees a linked list
+ * @head: pointer
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *current, temp;
+
+	if (!head)
+		return;
+	current = *head;
+
+	while (current)
+	{
+		temp = current;
+		current = current->next;
+		free(temp);
+	}
+	*head = NULL;
+}

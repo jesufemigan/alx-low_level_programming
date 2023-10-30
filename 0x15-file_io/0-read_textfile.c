1c814a133797dfaf6a098a36abc71b382f3060fe
Jesufemi <oladapofemmy@gmail.com>
2023-10-31 00:23:20 +0100

    task 0 done

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text fileand prints to POSIX standard output
+ *
+ * @filename: name of file
+ * @letters: number of letters to be printed
+ *
+ * Return: number of letters printed
+ *
+ */
+
+ssize_t read_textfile(const char *filename, ssize_t letters)
+{
+	char buf[BUF_SIZE];
+	ssize_t bytes;
+	int fd;
+
+	if (!filename | !letters)
+		return (0);
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+	bytes = read(fd, buf, letters);
+	bytes = write(STDOUT_FILENO, buf, bytes);
+	close(fd);
+	return (bytes);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <fnctl.h>
+
+#define BUF_SIZE 1024
+
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, ssize_t letters);
+
+#endif
